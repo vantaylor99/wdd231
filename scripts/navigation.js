fc0897b280cb36e30fd40cba5448fc54500a017d
vantaylor99 <jtaylor8@byupathway.edu>
2025-11-01 09:55:38 -0600

    updating site. committing so I can go and edit the code on another computer.

---
+++ scripts/navigation.js
@@ -0,0 +1,19 @@
+const navButton = document.querySelector('#nav-button');
+const navLinks = document.querySelector('#nav-bar')
+const navPages = document.querySelector('#nav-pages')
+
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navLinks.classList.toggle('show');
+})
+
+navPages.addEventListener('click', () => {
+    navPages.classList.toggle('current');
+
+})
+
+
+
+const navigation = document.getElementById('nav-bar');
+
+console.log(navigation)
