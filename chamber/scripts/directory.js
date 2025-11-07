c6d757f8a57f018ea7f625b04f91cefc553f9d2d
vantaylor99 <jacob.van.taylor@gmail.com>
2025-11-06 21:32:21 -0700

    javaa script update test

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,10 +1,12 @@
+const url = 'https://vantaylor99.github.io/wdd231/chamber/data/members.json';
 const cards = document.querySelector('#directory-cards');
 
 
 async function getMemberData() {
-    const response = await fetch();
+    const response = await fetch(url);
     const data = await response.json();
-    displayProphets(data.prophets)
+    console.log(data)
+    displayProphets(data.companies)
 }
 
 const prophets = getProphetData();
@@ -12,34 +14,34 @@ const prophets = getProphetData();
 console.log(prophets)
 
 
-const displayProphets = (prophets) => {
-    prophets.forEach(prophet => {
-        let card = document.createElement('section');
-        let fullName = document.createElement('h2');
-        let portrait = document.createElement('img');
-        let DOB = document.createElement('p');
-        let birthLocation = document.createElement('p');
+// const displayProphets = (prophets) => {
+//     prophets.forEach(prophet => {
+//         let card = document.createElement('section');
+//         let fullName = document.createElement('h2');
+//         let portrait = document.createElement('img');
+//         let DOB = document.createElement('p');
+//         let birthLocation = document.createElement('p');
 
-        card.classList.add('prophet-card')
-
-
-        fullName.textContent = `${prophet.name} ${prophet.lastname}`
-
-        portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', `A picture of ${prophet.name} ${prophet.lastname}, a prophet for the Church of Jesus Christ of Latter-day Saints.`);
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
-
-        DOB.textContent = `Date of Birth: ${prophet.birthdate}`;
-        birthLocation.textContent = `Place of Birth: ${prophet.birthplace}`
+//         card.classList.add('prophet-card')
 
 
-        card.appendChild(fullName);
-        card.appendChild(DOB);
-        card.appendChild(birthLocation);
-        card.appendChild(portrait);
+//         fullName.textContent = `${prophet.name} ${prophet.lastname}`
 
-        cards.appendChild(card);
-    });
-}
+//         portrait.setAttribute('src', prophet.imageurl);
+//         portrait.setAttribute('alt', `A picture of ${prophet.name} ${prophet.lastname}, a prophet for the Church of Jesus Christ of Latter-day Saints.`);
+//         portrait.setAttribute('loading', 'lazy');
+//         portrait.setAttribute('width', '340');
+//         portrait.setAttribute('height', '440');
+
+//         DOB.textContent = `Date of Birth: ${prophet.birthdate}`;
+//         birthLocation.textContent = `Place of Birth: ${prophet.birthplace}`
+
+
+//         card.appendChild(fullName);
+//         card.appendChild(DOB);
+//         card.appendChild(birthLocation);
+//         card.appendChild(portrait);
+
+//         cards.appendChild(card);
+//     });
+// }
