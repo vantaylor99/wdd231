ff492dd6db3a11e265dc7e387c632cdc2bc0ade3
vantaylor99 <jtaylor8@byupathway.edu>
2025-11-24 18:41:18 -0700

    finishing touches

--- chamber/scripts/last-visit.mjs
+++ chamber/scripts/last-visit.mjs
@@ -1,6 +1,4 @@
-
-
-// const dateToday = new Date();
+const welcomeDiv = document.getElementById('welcome-popup');
 const msToDays = 86400000;
 const todayMs = Date.now();
 
@@ -21,7 +19,49 @@ function getLastVisit() {
         return Number(lastVisit);
     }
 }
-const daysSince = Math.floor(((todayMs - getLastVisit()) / msToDays));
 
 
-console.log(daysSince);
+const daysSince = ((todayMs - getLastVisit()) / msToDays);
+const welcomeMessage = document.createElement('h3');
+const welcomeButton = document.createElement('button');
+welcomeButton.classList.add('button-common')
+
+function getWelcomeMessage() {
+    let message = '';
+    if (daysSince === 0) {
+        message = 'Welcome! Let us know if you have any questions!'
+    }
+    else if (daysSince < 1) {
+        message = 'Back so soon? Awesome!'
+    }
+    else {
+        let plural = 'days'
+        const wholeNumber = Math.floor(daysSince);
+        console.log(wholeNumber)
+        if (wholeNumber === 1) {
+            plural = ''
+            message = `You last visited ${wholeNumber} day${plural} ago.`
+        }
+        else {
+            plural = 's'
+            message = `You last visited ${wholeNumber} day${plural} ago.`
+        }
+    }
+    return message;
+}
+
+function displayWelcomeMessage(message) {
+    welcomeMessage.textContent = message;
+    welcomeButton.textContent = "✕";
+    welcomeDiv.appendChild(welcomeMessage);
+    welcomeDiv.appendChild(welcomeButton);
+}
+displayWelcomeMessage(getWelcomeMessage());
+
+document.addEventListener('DOMContentLoaded', () => {
+    welcomeDiv.style.display = 'grid';
+
+    welcomeButton.addEventListener('click', () => {
+        welcomeDiv.style.display = 'none';
+    })
+})
