6e41590d82f36c9be3b415cea37b527fd0230c5f
vantaylor99 <jtaylor8@byupathway.edu>
2025-12-08 18:44:39 -0700

    one tiny fix

--- toTheMoon/scripts/set-moon-values.mjs
+++ toTheMoon/scripts/set-moon-values.mjs
@@ -5,4 +5,4 @@ import { getMoonData } from "./moon-distance-data.mjs";
 const moonDistanceSpan = document.getElementById('moon-distance-span');
 const moonData = await getMoonData()
 
-moonDistanceSpan.innerHTML = `<strong>${Number(moonData.moon_distance).toFixed(2)} km. </strong>`
+moonDistanceSpan.innerHTML = `<strong>${Number(moonData.moon_distance).toLocaleString()} km. </strong>`
